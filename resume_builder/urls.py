a45124d557b462886cefe96a2836649a4c52b20c
Shannon Elsner <47678866+selsner@users.noreply.github.com>
2020-09-22 17:08:25 -0700

    Update urls.py

--- resume_builder/urls.py
+++ resume_builder/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.resume_form, name='resume_form'),
+    path('', views.home, name='home'),
+    path('choose_template/', views.choose_template, name='choose_template'),
+    path('resume_form/', views.resume_form, name='resume_form'),
     path('resume/<str:pk>/', views.resume, name='resume'),
     path('download_pdf/', views.download_pdf, name='download_pdf'),
 ]
