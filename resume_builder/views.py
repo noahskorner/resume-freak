88035ab38c026e86aa0497efdeafa8504c0e5f4b
maxwforward <43326454+maxwforward@users.noreply.github.com>
2020-11-02 22:06:14 -0800

    Restrict access to login/register pages if user is already signed in

--- resume_builder/views.py
+++ resume_builder/views.py
@@ -7,6 +7,7 @@ from django.template.loader import get_template
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required #@login_required(login_url='login')
 from .models import *
 import uuid
 
@@ -16,30 +17,37 @@ def home(request):
     return render(request, 'resume_builder/home.html')
 
 def registerPage(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
-            return redirect('login')
-    context = {'form': form}
-    return render(request, 'resume_builder/register.html', context)
+    # If user is already logged in, restrict access to register page 
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'resume_builder/register.html', context)
 
 def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password) 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username OR password is incorrect')
-
-    context = {}
-    return render(request, 'resume_builder/login.html', context)
+    # If user is already logged in, restrict access to register page 
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password) 
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+        context = {}
+        return render(request, 'resume_builder/login.html', context)
 
 def logoutUser(request):
     logout(request)
